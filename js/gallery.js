1b62f337bd035692902a6281f5b49606eaa80035
Brian Dennis <briandennis321@gmail.com>
2016-04-01 09:39:34 -0500

    added lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,13 @@
 import Bricks from 'bricks.js';
+
+const sizes = [
+  { columns: 2, gutter: 10 },
+  { mq: '768px', columns: 3, gutter: 25 },
+  { mq: '1024px', columns: 4, gutter: 50 }
+]
+
+const instance = Bricks({
+  container: '#gallery',
+  packed: 'data-packed',
+
+});
